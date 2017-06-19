53c03be200ca2aae264c91d85c3715100b09ee66
Helge Eichhorn <git@helgeeichhorn.de>
2017-06-19 17:12:29 +0200

    Initial import

--- src/ModelParameters.jl
+++ src/ModelParameters.jl
@@ -1,5 +1,128 @@
 module ModelParameters
 
-# package code goes here
+export AbstractParameter, Parameter, getparameters,
+    show, lower, upper, initial, value, constant, convert,
+    +, *, -, /, <, isequal, isapprox, one, zero, norm, real, imag,
+    isparameter, isfixed, reset!
+
+import Base: +, *, -, /, \, ==, <, promote_rule, convert, push!, isequal,
+    one, oneunit, zero, norm, real, imag, isapprox
+
+abstract type AbstractParameter <: Number end
+
+mutable struct Parameter{T<:Number} <: AbstractParameter
+    value::T
+    initial::T
+    lower::T
+    upper::T
+    isfixed::Bool
+    function Parameter(initial::T, lower::T, upper::T) where T<:Number
+        if lower > upper
+            throw(ArgumentError(
+                "Invalid interval: lower bound is greater than upper bound."))
+        elseif initial < lower || initial > upper
+            throw(ArgumentError(
+                "Initial value is outside the interval."))
+        end
+        isfixed = lower ≈ upper
+        new{T}(initial, initial, lower, upper, isfixed)
+    end
+end
+
+Parameter(v::T) where {T} = Parameter(v, typemin(T), typemax(T))
+Parameter(lower, upper) = Parameter(lower + (upper - lower) / 2, lower, upper)
+constant(v) = Parameter(v, v, v)
+
+function push!(prm::Parameter, v)
+    if v < lower(prm) || v > upper(prm)
+        throw(ArgumentError("$v is outside the interval."))
+    end
+    prm.value = v
+    return prm
+end
+
+function reset!(prm::Parameter)
+    prm.value = prm.initial
+    return prm
+end
+
+lower(par::Parameter) = par.lower
+upper(par::Parameter) = par.upper
+initial(par::Parameter) = par.initial
+value(par::Parameter) = par.value
+isfixed(prm::Parameter) = prm.isfixed
+isparameter(prm::Parameter) = !prm.isfixed
+one(::Type{Parameter{T}}) where {T} = constant(one(T))
+oneunit(::Type{Parameter{T}}) where {T} = constant(oneunit(T))
+zero(::Type{Parameter{T}}) where {T} = constant(zero(T))
+one(::Parameter{T}) where {T} = constant(one(T))
+oneunit(::Parameter{T}) where {T} = constant(oneunit(T))
+zero(::Parameter{T}) where {T} = constant(zero(T))
+norm(p::Parameter) = p.value
+real(p::Parameter) = p.value
+imag(p::Parameter) = 0.0
+
+convert(::Type{Parameter{T}}, v::T) where T<:Number = constant(v)
+promote_rule(::Type{Parameter{T}}, ::Type{T}) where T<:Number = Parameter{T}
+
+Base.show(io::IO, prm::Parameter) = prm.isfixed ? print(io, prm.value) :
+    print(io, prm.lower, " ≤ ", prm.value, " ≤ ", prm.upper)
+
+(+)(lhs::Parameter, rhs::Parameter) = lhs.value + rhs.value
+(-)(lhs::Parameter, rhs::Parameter) = lhs.value - rhs.value
+(*)(lhs::Parameter, rhs::Parameter) = lhs.value * rhs.value
+(/)(lhs::Parameter, rhs::Parameter) = lhs.value / rhs.value
+
+(+)(lhs::Parameter, rhs::Number) = lhs.value + rhs
+(+)(lhs::Number, rhs::Parameter) = lhs + rhs.value
+(-)(lhs::Parameter, rhs::Number) = lhs.value - rhs
+(-)(lhs::Number, rhs::Parameter) = lhs - rhs.value
+(*)(lhs::Parameter, rhs::Number) = lhs.value * rhs
+(*)(lhs::Number, rhs::Parameter) = lhs * rhs.value
+(/)(lhs::Parameter, rhs::Number) = lhs.value / rhs
+(/)(lhs::Number, rhs::Parameter) = lhs / rhs.value
+
+isequal(lhs::Parameter, rhs::Parameter) = isequal(lhs.value, rhs.value)
+isequal(lhs::Number, rhs::Parameter) = isequal(lhs, rhs.value)
+isequal(lhs::Parameter, rhs::Number) = isequal(lhs.value, rhs)
+==(lhs::Parameter, rhs::Parameter) = ==(lhs.value, rhs.value)
+==(lhs::Number, rhs::Parameter) = ==(lhs, rhs.value)
+==(lhs::Parameter, rhs::Number) = ==(lhs.value, rhs)
+(<)(lhs::Parameter, rhs::Parameter) = lhs.value < rhs.value
+(<)(lhs::Number, rhs::Parameter) = lhs < rhs.value
+(<)(lhs::Parameter, rhs::Number) = lhs.value < rhs
+isapprox(lhs::Parameter, rhs::Parameter) = isapprox(lhs.value, rhs.value)
+isapprox(lhs::Number, rhs::Parameter) = isapprox(lhs, rhs.value)
+isapprox(lhs::Parameter, rhs::Number) = isapprox(lhs.value, rhs)
+
+const fun = (
+    :abs2, :acosh, :acot, :acotd, :acoth, :acsc, :acscd, :acsch, :asec,
+    :asecd, :asinh, :atan, :atand, :cbrt, :cos, :cosd, :cosh, :cot, :cotd,
+    :coth, :csc, :cscd, :csch, :exp, :exp2, :expm1, :gamma, :inv, :lgamma,
+    :log, :log10, :log1p, :log2, :sec, :secd, :sech, :sin, :sind, :sinh,
+    :sqrt, :tan, :tand, :tanh, :acos, :acosd, :asech, :asin, :asind, :atanh,
+)
+for f in fun
+    @eval begin
+        import Base.$f
+        export $f
+        $f(par::Parameter) = $f(par.value)
+    end
+end
+
+getparameters(data) = getparameters(Parameter[], data)
+function getparameters(params, prm::Parameter)
+    isparameter(prm) && push!(params, prm)
+    return params
+end
+function getparameters(params, arr::AbstractArray)
+    foreach(x -> getparameters(params, x), arr)
+    return params
+end
+function getparameters(params, val)
+    fields = fieldnames(val)
+    foreach(x -> getparameters(params, getfield(val, x)), fields)
+    return params
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,125 @@
 using ModelParameters
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+@testset "ModelParameters" begin
+    @test_throws ArgumentError Parameter(0.0, 1.0, -1.0)
+    @test_throws ArgumentError Parameter(-2.0, -1.0, 1.0)
+    p = Parameter(0.0)
+    @test lower(p) == -Inf
+    @test upper(p) == Inf
+    p = Parameter(0)
+    @test lower(p) == typemin(Int)
+    @test upper(p) == typemax(Int)
+    p = Parameter(-1.0, 1.0)
+    @test initial(p) == 0.0
+    @test isparameter(p)
+    c = constant(1.0)
+    @test !isparameter(c)
+    @test isfixed(c)
+    @test_throws ArgumentError push!(p, 3.0)
+    @test_throws ArgumentError push!(p, -3.0)
+    push!(p, 0.5)
+    @test p == 0.5
+    @test p ≈ 0.5
+    reset!(p)
+    @test 0 == p
+    @test 0 ≈ p
+    arr = [p, 1.0]
+    @test isparameter.(arr) == [true, false]
+    @test value.(arr) == [0.0, 1.0]
+    @test getparameters(p) == [p]
+    @test getparameters(c) == Parameter[]
+    @test getparameters(arr) == [p]
+    @test getparameters([Nullable(p), Nullable(c)]) == [p]
+    @test arr ≈ arr
+
+    p = one(p)
+    @test norm(p) == value(p)
+    @test imag(p) == 0im
+    @test isequal(1.0, p)
+    @test isequal(p, 1.0)
+    @test isapprox(p, p)
+    @test isapprox(1.0, p)
+    @test zero(p) == 0.0
+
+    p = Parameter(0.0)
+    @test string(p) == "-Inf ≤ 0.0 ≤ Inf"
+    @test string(c) == "1.0"
+
+    @test c + c == 2
+    @test c - c == 0
+    @test c * c == 1
+    @test c / c == 1
+
+    @test c + 1 == 2
+    @test c - 1 == 0
+    @test c * 1 == 1
+    @test c / 1 == 1
+    @test 1 + c == 2
+    @test 1 - c == 0
+    @test 1 * c == 1
+    @test 1 / c == 1
+
+    a = [1.0, 2.0]
+    @test c + a == [2.0, 3.0]
+    @test c - a == [0.0, -1.0]
+    @test c * a == [1.0, 2.0]
+    @test a + c == [2.0, 3.0]
+    @test a - c == [0.0, 1.0]
+    @test a * c == [1.0, 2.0]
+    @test a / c == [1.0, 2.0]
+    @test isequal(p,p)
+    @test !isequal(p,c)
+
+    @test arr+3 == [3.0, 4.0]
+    @test 3+arr == [3.0, 4.0]
+    @test arr-3 == [-3.0, -2.0]
+    @test 3-arr == [3.0, 2.0]
+    @test arr*3 == [0.0, 3.0]
+    @test 3*arr == [0.0, 3.0]
+    @test arr/3 == [0.0, 1/3.0]
+    @test p < c
+    @test c > p
+    @test -1 < c
+    @test c < 3
+    @test c > -1
+    @test 3 > c
+
+    @test [c, c] + [c, c] == [1, 1] + [1, 1]
+    @test [1, 1] + [c, c] == [1, 1] + [1, 1]
+    @test [c, c] + [1, 1] == [1, 1] + [1, 1]
+    @test [c, c] - [c, c] == [1, 1] - [1, 1]
+    @test [1, 1] - [c, c] == [1, 1] - [1, 1]
+    @test [c, c] - [1, 1] == [1, 1] - [1, 1]
+
+    @test [c, c] .* [c, c] == [1, 1] .* [1, 1]
+    @test [1, 1] .* [c, c] == [1, 1] .* [1, 1]
+    @test [c, c] .* [1, 1] == [1, 1] .* [1, 1]
+    @test [c, c] ./ [c, c] == [1, 1] ./ [1, 1]
+    @test [1, 1] ./ [c, c] == [1, 1] ./ [1, 1]
+    @test [c, c] ./ [1, 1] == [1, 1] ./ [1, 1]
+
+    p = Parameter(0.0)
+    const fun = (
+        :abs2, :acosh, :acot, :acotd, :acoth, :acsc, :acscd, :acsch, :asec,
+        :asecd, :asinh, :atan, :atand, :cbrt, :cos, :cosd, :cosh, :cot, :cotd,
+        :coth, :csc, :cscd, :csch, :exp, :exp2, :expm1, :gamma, :inv, :lgamma,
+        :log, :log10, :log1p, :log2, :sec, :secd, :sech, :sin, :sind, :sinh,
+        :sqrt, :tan, :tand, :tanh,
+    )
+    const fun2 = (:acos, :acosd, :asech, :asin, :asind, :atanh)
+    val = pi/2
+    push!(p, val)
+    for f in fun
+        @eval begin
+            @test $f($p) == $f(float($val))
+        end
+    end
+    val = 0.0
+    push!(p, val)
+    for f in fun2
+        @eval begin
+            @test $f($p) == $f(float($val))
+        end
+    end
+end
